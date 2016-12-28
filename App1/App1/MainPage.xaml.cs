36ad293819e18d0c910fe21a0a339bc43dd19939
kevingoebel <kgoebel1988@gmail.com>
2016-12-28 16:26:39 +0100

    test commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,148 @@
+﻿using App1.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer timerDateTime;
+        DispatcherTimer timerWeather;
+        DailyTemp dailyWeather;
+        ForeCast foreCast;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FillControlls();
+            //SetImageTick();
+        }
+        private async void FillControlls()
+        {
+            dailyWeather = await OpenWeather.GetDailyWeather(50.95, 5.97);
+            foreCast = await OpenWeather.GetForeCastWeather(50.95, 5.97, 5);
+
+            SetTimer();
+            SetWeather();
+            SetDateTime();
+            SetForeCast();
+        }
+        private void SetTimer()
+        {
+            timerDateTime = new DispatcherTimer();
+            timerDateTime.Tick += timerDateTime_Tick;
+            timerDateTime.Interval = new TimeSpan(0, 0, 1);
+            timerDateTime.Start();
+
+            timerWeather = new DispatcherTimer();
+            timerWeather.Tick += TimerWeather_Tick;
+            timerWeather.Interval = new TimeSpan(1, 0, 0);
+            timerWeather.Start();
+        }
+
+        private void TimerWeather_Tick(object sender, object e)
+        {
+            SetWeather();
+            SetForeCast();
+        }
+
+        private void timerDateTime_Tick(object sender, object e)
+        {
+            SetDateTime();
+        }
+        private void SetWeather()
+        {
+            var description = dailyWeather.weather[0].description;
+
+            weatherImage.Source = SetImage(dailyWeather.weather[0].main);
+            lblTemp.Text = $"{dailyWeather.main.temp.ToString("0.0")}°";
+            lblWeatherType.Text = char.ToUpper(description[0]) + description.Substring(1);
+            lblWeatherCity.Text = dailyWeather.name;
+        }
+        private void SetDateTime()
+        {
+            lblClock.Text = DateTime.Now.ToString("HH:mm:ss");
+            lblCalender.Text = DateTime.Now.ToString("dd-MMMM-yyyy");
+        }
+        private void SetForeCast()
+        {
+            lblForeCastDay1.Text = Enum.GetName(typeof(DayOfWeek), GetIntOfWeek(1));
+            lblForeCastDay2.Text = Enum.GetName(typeof(DayOfWeek), GetIntOfWeek(2));
+            lblForeCastDay3.Text = Enum.GetName(typeof(DayOfWeek), GetIntOfWeek(3));
+            lblForeCastDay4.Text = Enum.GetName(typeof(DayOfWeek), GetIntOfWeek(4));
+
+            lblForeCast1.Text = $"{foreCast.list[1].temp.max.ToString("0.0")}°";
+            lblForeCast2.Text = $"{foreCast.list[2].temp.max.ToString("0.0")}°";
+            lblForeCast3.Text = $"{foreCast.list[3].temp.max.ToString("0.0")}°";
+            lblForeCast4.Text = $"{foreCast.list[4].temp.max.ToString("0.0")}°";
+
+            lblForeCastIcon1.Source = SetImage(foreCast.list[1].weather[0].main);
+            lblForeCastIcon2.Source = SetImage(foreCast.list[2].weather[0].main);
+            lblForeCastIcon3.Source = SetImage(foreCast.list[3].weather[0].main);
+            lblForeCastIcon4.Source = SetImage(foreCast.list[4].weather[0].main);
+        }
+        private int GetIntOfWeek(int index)
+        {
+            var day = (int)DateTime.Now.DayOfWeek;
+
+            return (day + index) <= 6 ? (day + index) : (day + index) - 7;
+        }
+
+        private BitmapImage SetImage(string weather)
+        {
+            switch (weather)
+            {
+                case "Clear":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Sun.png"));
+                case "Rain":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Rain.png"));
+                case "Clouds":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Clouds.png"));
+                case "Mist":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Clouds.png"));
+                case "Fog":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Clouds.png"));
+                case "Snow":
+                    return new BitmapImage(new Uri("ms-appx:///Image/Snow.png"));
+                default:
+                    break;
+            }
+            return new BitmapImage(new Uri("ms-appx:///Image/Sun.png")); ;
+        }
+        //private async void SetImageTick()
+        //{
+        //    weatherImage.Source = new BitmapImage(new Uri("ms-appx:///Image/Sun.png"));
+        //    await Task.Delay(TimeSpan.FromSeconds(3));
+        //    weatherImage.Source = new BitmapImage(new Uri("ms-appx:///Image/Rain.png"));
+        //    await Task.Delay(TimeSpan.FromSeconds(3));
+        //    weatherImage.Source = new BitmapImage(new Uri("ms-appx:///Image/Clouds.png"));
+        //    await Task.Delay(TimeSpan.FromSeconds(3));
+        //    weatherImage.Source = new BitmapImage(new Uri("ms-appx:///Image/Snow.png"));
+        //}
+    }
+}
---
+++ App1/App1/Model/OpenWeather.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Model
+{
+    public class OpenWeather
+    {
+        public async static Task<DailyTemp> GetDailyWeather(double lat, double lon)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+
+                var response = await httpClient.GetAsync(new Uri($"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=a42b89cfb671188837b7ebbcb9558568&units=metric&lang=nl"));
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var serializer = new DataContractJsonSerializer(typeof(DailyTemp));
+
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+
+                var data = (DailyTemp)serializer.ReadObject(ms);
+                return data;
+            }
+        }
+        public async static Task<ForeCast> GetForeCastWeather(double lat, double lon, int cnt)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(new Uri($"http://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&APPID=a42b89cfb671188837b7ebbcb9558568&units=metric&lang=nl"));
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var serializer = new DataContractJsonSerializer(typeof(ForeCast));
+
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+
+                var data = (ForeCast)serializer.ReadObject(ms);
+                return data;
+            }
+        }
+    }
+
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public double lat { get; set; }
+    }
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string main { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+    }
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+        [DataMember]
+        public int pressure { get; set; }
+        [DataMember]
+        public int humidity { get; set; }
+        [DataMember]
+        public double temp_min { get; set; }
+        [DataMember]
+        public double temp_max { get; set; }
+    }
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+        [DataMember]
+        public int deg { get; set; }
+    }
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public int type { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public double message { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public int sunrise { get; set; }
+        [DataMember]
+        public int sunset { get; set; }
+    }
+    [DataContract]
+    public class DailyTemp
+    {
+        [DataMember]
+        public Coord coord { get; set; }
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public string @base { get; set; }
+        [DataMember]
+        public Main main { get; set; }
+        [DataMember]
+        public int visibility { get; set; }
+        [DataMember]
+        public Wind wind { get; set; }
+        [DataMember]
+        public Clouds clouds { get; set; }
+        [DataMember]
+        public int dt { get; set; }
+        [DataMember]
+        public Sys sys { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public int cod { get; set; }
+    }
+    [DataContract]
+    public class City
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public Coord coord { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public int population { get; set; }
+        [DataMember]
+        public int geoname_id { get; set; }
+        [DataMember]
+        public double lat { get; set; }
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public string iso2 { get; set; }
+        [DataMember]
+        public string type { get; set; }
+    }
+    [DataContract]
+    public class ForeCast
+    {
+        [DataMember]
+        public City city { get; set; }
+        [DataMember]
+        public string cod { get; set; }
+        [DataMember]
+        public double message { get; set; }
+        [DataMember]
+        public int cnt { get; set; }
+        [DataMember]
+        public List<List> list { get; set; }
+    }
+    [DataContract]
+    public class List
+    {
+        [DataMember]
+        public int dt { get; set; }
+        [DataMember]
+        public Temp temp { get; set; }
+        [DataMember]
+        public double pressure { get; set; }
+        [DataMember]
+        public int humidity { get; set; }
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public double speed { get; set; }
+        [DataMember]
+        public int deg { get; set; }
+        [DataMember]
+        public int clouds { get; set; }
+        [DataMember]
+        public double? rain { get; set; }
+        [DataMember]
+        public double? snow { get; set; }
+    }
+    [DataContract]
+    public class Temp
+    {
+        [DataMember]
+        public double day { get; set; }
+        [DataMember]
+        public double min { get; set; }
+        [DataMember]
+        public double max { get; set; }
+        [DataMember]
+        public double night { get; set; }
+        [DataMember]
+        public double eve { get; set; }
+        [DataMember]
+        public double morn { get; set; }
+    }
+}
